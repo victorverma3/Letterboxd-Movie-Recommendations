a31e99873eac568158aff22845e851de2cbac3c4
victor <vpverm@bu.edu>
2024-05-02 10:03:57 -0400

    fixed mobile navbar click range and issue with menu not closing

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
     const [nav, setNav] = useState(false);
-    const [brand, setBrand] = useState(true);
 
     const handleNav = () => {
         setNav(!nav);
-        setBrand(!brand);
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 640 && nav) {
+                setNav(false);
+            }
+        };
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [nav]);
 
     const navItems = [
         { id: 1, text: "Home", url: "/" },
@@ -19,16 +29,9 @@ const Header = () => {
 
     return (
         <div className="mx-auto px-4 flex justify-between items-center max-w-[1240px]">
-            {brand ? (
-                <h1 className="w-full text-3xl text-amber-800 font-bold">
-                    <a href="/">LMR</a>
-                </h1>
-            ) : (
-                <h1 className="w-full text-3xl text-white font-bold">
-                    <a href="/">LMR</a>
-                </h1>
-            )}
-
+            <h1 className="w-full text-3xl font-bold text-amber-800">
+                <a href="/">LMR</a>
+            </h1>
             <ul className="hidden sm:flex">
                 {navItems.map((item) => (
                     <li
@@ -49,11 +52,9 @@ const Header = () => {
             </div>
 
             <ul
-                className={
-                    nav
-                        ? "w-3/5 h-full z-50 bg-white fixed sm:hidden top-0 left-0 ease-in-out duration-300"
-                        : "w-3/5 h-full z-50 fixed top-0 bottom-0 left-[-100%] ease-in-out duration-300"
-                }
+                className={`w-3/6 h-full z-50 fixed top-0 bottom-0 ease-in-out duration-300 ${
+                    nav ? "bg-white sm:hidden left-0" : "left-[-100%]"
+                }`}
             >
                 <li className="w-full m-4 text-3xl font-bold text-amber-800">
                     LMR
@@ -61,7 +62,12 @@ const Header = () => {
 
                 {navItems.map((item) => (
                     <li className="p-4 border-b hover:bg-amber-800 duration-300 hover:text-black cursor-pointer border-gray-600">
-                        <Link key={item.id} onClick={handleNav} to={item.url}>
+                        <Link
+                            className="block w-full"
+                            key={item.id}
+                            onClick={handleNav}
+                            to={item.url}
+                        >
                             {item.text}
                         </Link>
                     </li>
