f100877f2254521ecb866a8bebd95467ac60e6cf
victor <vpverm@bu.edu>
2024-05-02 13:45:49 -0400

    fixed movie data and statistics update

--- backend/data_processing/scrape_user_ratings.py
+++ backend/data_processing/scrape_user_ratings.py
@@ -72,12 +72,6 @@ async def get_user_ratings(user, session, verbose=True):
     # updates user ratings data
     if len(user_df) > 4:
 
-        try:
-            database.update_user_log(user)
-            print(f"\nsuccessfully logged user in database")
-        except:
-            print(f"\nfailed to log user in database")
-
         # creates urls df
         urls_df = pd.DataFrame({"movie_id": ids, "title": titles, "url": urls})
 
