811ae5aa7ad04c0c4e26e7904898d619ba5d4943
victor <vpverm@bu.edu>
2024-05-18 19:06:45 -0400

    implemented statistics update

--- backend/data_processing/utility.py
+++ backend/data_processing/utility.py
@@ -10,6 +10,7 @@ from data_processing.scrape_user_ratings import get_user_ratings
 
 
 async def get_user_dataframe(user):
+
     # gets and processes all movie data
     movie_data = database.get_movie_data()
     movie_data["title"] = movie_data["title"].astype("string")
@@ -18,16 +19,19 @@ async def get_user_dataframe(user):
     # gets and processes the user data
     try:
         async with aiohttp.ClientSession() as session:
-            user_df, unrated = await get_user_ratings(user, session, False)
+            user_df, _ = await get_user_ratings(user, session, False)
         user_df["movie_id"] = user_df["movie_id"].astype("int")
         user_df["url"] = user_df["url"].astype("string")
         user_df["username"] = user_df["username"].astype("string")
-    except ValueError as e:
+
+        processed_user_df = user_df.merge(
+            movie_data, how="left", on=["movie_id", "url"]
+        )
+        processed_user_df["rating_differential"] = (
+            processed_user_df["user_rating"] - processed_user_df["letterboxd_rating"]
+        )
+
+        return processed_user_df
+    except Exception as e:
+        print(f"\nerror getting user dataframe")
         raise e
-
-    processed_user_df = user_df.merge(movie_data, how="left", on=["movie_id", "url"])
-    processed_user_df["rating_differential"] = (
-        processed_user_df["user_rating"] - processed_user_df["letterboxd_rating"]
-    )
-
-    return processed_user_df
