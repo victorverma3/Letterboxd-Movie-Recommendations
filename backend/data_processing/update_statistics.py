a1d516e44cb4def41a8fecbfe7571d8f152abe74
victor <vpverm@bu.edu>
2024-05-22 17:47:39 -0400

    optimizations

--- backend/data_processing/update_statistics.py
+++ backend/data_processing/update_statistics.py
@@ -15,20 +15,36 @@ async def statistics_update():
 
     start = time.perf_counter()
 
+    # gets statistics users from database
     try:
-        # gets statistics users from database
         statistics_users = database.get_statistics_user_log()
     except Exception as e:
-        print(f"\nfailed to get statistics users")
+        print("\nfailed to get statistics users")
         raise e
 
-    # updates user statistics in database
+    # gets movie data from database
+    try:
+        movie_data = database.get_movie_data()
+    except Exception as e:
+        print("\nfailed to get movie data")
+        raise e
+
+    # gets all updated user statistics
+    all_stats = {}
     for user in statistics_users:
         try:
-            user_df = await get_user_dataframe(user)
-            await get_user_statistics(user, user_df)
+            user_df = await get_user_dataframe(user, movie_data)
+            all_stats[user] = get_user_statistics(user_df)
+            print(f"\nsuccessfully calculated {user}'s statistics")
         except:
-            print(f"\nfailed to update {user}'s statistics")
+            print(f"\nfailed to get {user}'s statistics")
+
+    # updates user data in database
+    try:
+        database.update_many_user_statistics(all_stats)
+        print(f"\nsuccessfully updated all user statistics in database")
+    except:
+        print(f"\nfailed to update all user statistics in database")
 
     finish = time.perf_counter()
     print(f"\nupdated statistics in {finish - start} seconds")
