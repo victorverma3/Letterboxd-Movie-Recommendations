ce191f60105fae7f5c7da5082ec54c0545607b58
victor <vpverm@bu.edu>
2024-05-23 00:06:13 -0400

    implemented watchlist picker

--- frontend/src/pages/Watchlist.tsx
+++ frontend/src/pages/Watchlist.tsx
@@ -1,5 +1,14 @@
+import Picks from "../components/Picks";
+
 const Watchlist = () => {
-    return <div className="w-fit mx-auto">Coming Soon</div>;
+    return (
+        <div>
+            <h1 className="w-96 max-w-full mx-auto mt-16 text-center text-4xl">
+                Letterboxd Watchlist Picker
+            </h1>
+            <Picks />
+        </div>
+    );
 };
 
 export default Watchlist;
