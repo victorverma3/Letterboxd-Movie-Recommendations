f28ec2fd55a3619a123477a2575b8bc18ea2ab0c
victor <vpverm@bu.edu>
2024-05-28 16:03:39 -0400

    watchlist page back online

--- frontend/src/pages/Watchlist.tsx
+++ frontend/src/pages/Watchlist.tsx
@@ -7,13 +7,15 @@ const Watchlist = () => {
             <h1 className="w-96 max-w-full mx-auto mt-16 text-center text-4xl">
                 Letterboxd Watchlist Picker
             </h1>
-            <div className="w-96 max-w-full mx-auto my-8">
-                <Maintenance
-                    severity="warning"
-                    message="The site is currently undergoing maintenance to increase optimization. Watchlist picker is temporarily disabled until 5/29. Sorry for the inconvenience!"
-                />
-            </div>
-            {false && <Picks />}
+            {false && (
+                <div className="w-96 max-w-full mx-auto my-8">
+                    <Maintenance
+                        severity="warning"
+                        message="The site is currently undergoing maintenance to increase optimization. Watchlist picker is temporarily disabled until 5/29. Sorry for the inconvenience!"
+                    />
+                </div>
+            )}
+            <Picks />
             <p className="mx-auto my-4 text-center">
                 Follow my{" "}
                 <a
