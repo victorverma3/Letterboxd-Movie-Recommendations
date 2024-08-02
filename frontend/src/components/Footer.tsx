05aa84328d1197c86d0e51e68f8e28612931f6c8
victor <vpverm@bu.edu>
2024-08-02 00:20:28 -0400

    minor ui tweaks

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
                 >
                     Victor Verma
                 </a>{" "}
-                | v1.0.0
+                | v1.0.1
             </p>
             <div className="w-40 mx-auto flex flex-row justify-around">
                 {logos.map((logo, index) => (
