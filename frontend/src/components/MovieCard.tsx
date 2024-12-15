eec2a9de471be7283108dd06aaeba99a7e1e52c8
victor <vpverm@bu.edu>
2024-12-14 19:19:42 -0500

    made movie card slightly wider on mobile

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -16,7 +16,7 @@ const MovieCard = ({
     url,
 }: MovieCardProps) => {
     return (
-        <div className="w-32 sm:w-48 mt-4 flex flex-col border-2 border-gray-200 rounded-lg duration-200 hover:scale-105 hover:transition hover:border-amber-800 hover:shadow-lg">
+        <div className="w-36 sm:w-48 mt-4 flex flex-col border-2 border-gray-200 rounded-lg duration-200 hover:scale-105 hover:transition hover:border-amber-800 hover:shadow-lg">
             <Link
                 to={url}
                 target="_blank"
