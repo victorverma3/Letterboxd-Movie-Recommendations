ddaa73c3b35f37be02de0dc100cd317488541f6a
victor <vpverm@bu.edu>
2025-02-01 14:31:43 -0500

    added alert

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -3,14 +3,17 @@ import { Analytics } from "@vercel/analytics/react";
 
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
-// import CustomAlert from "./components/CustomAlert";
+import CustomAlert from "./components/Alerts/CustomAlert";
 
 function Layout() {
     return (
         <>
             <Header />
             <div className="flex flex-col min-h-screen">
-                {/* <CustomAlert severity="info" message="" /> */}
+                <CustomAlert
+                    severity="error"
+                    message="The site is currently experiencing technical difficulties. I am investigating and will try to fix it as soon as possible. Sorry for the inconvenience!"
+                />
                 <div className="grow">
                     <Outlet />
                 </div>
