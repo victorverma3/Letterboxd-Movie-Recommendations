2beac9ec510cb6412e9809a1b2c0153e8e4b5827
victor <vpverm@bu.edu>
2025-03-13 15:08:11 -0400

    frontend functionality for watchlist recommendations

--- frontend/src/types/WatchlistTypes.ts
+++ frontend/src/types/WatchlistTypes.ts
@@ -4,14 +4,24 @@ export type PickFormValues = {
     numPicks: number;
 };
 
-export type PickResponse = {
+export type PickResponseBrief = {
+    title: string;
+    url: string;
+};
+
+export type PickType = "random" | "recommendation";
+
+export type RandomPickResponse = {
     title: string;
     poster: string;
     url: string;
     release_year: number;
 };
 
-export type PickResponseBrief = {
+export type RecommendationPickResponse = {
     title: string;
+    poster: string;
+    release_year: number;
+    predicted_rating: number;
     url: string;
 };
