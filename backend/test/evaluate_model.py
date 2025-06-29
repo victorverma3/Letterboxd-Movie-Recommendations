a436306449816399667eb41b6c6846b8f79eac50
victor <vpverm@bu.edu>
2025-06-29 16:49:47 -0400

    renamed utility.py -> utils.py

--- backend/test/evaluate_model.py
+++ backend/test/evaluate_model.py
@@ -13,7 +13,7 @@ sys.path.append(project_root)
 
 import data_processing.database as database
 from data_processing.scrape_user_ratings import get_user_ratings
-from data_processing.utility import process_genres, UserProfileException
+from backend.data_processing.utils import process_genres, UserProfileException
 from model.recommender import train_model
 
 
