576f2b77de23dc0000940a98bfa19e3ec8343052
Victor Verma <victor.verma@hotmail.com>
2025-08-11 22:51:12 -0400

    updated ratings and disabled update action

--- backend/model/update_rating_data.py
+++ backend/model/update_rating_data.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 import time
@@ -9,13 +10,49 @@ from data_processing import database
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+
+    # Month
+    parser.add_argument(
+        "-m",
+        "--month",
+        help="The month of the dataset.",
+        choices=[
+            "jan",
+            "feb",
+            "mar",
+            "apr",
+            "may",
+            "jun",
+            "jul",
+            "aug",
+            "sep",
+            "oct",
+            "nov",
+            "dec",
+        ],
+        required=True,
+    )
+
+    # Year
+    parser.add_argument(
+        "-y",
+        "--year",
+        help="The year of the dataset.",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
     start = time.perf_counter()
 
     # Loads user ratings
     user_ratings = database.get_user_ratings()
 
     # Updates user ratings
-    user_ratings.to_csv("../data/training/user_ratings.csv", index=False)
+    user_ratings.to_csv(
+        f"../data/training/user_ratings_{args.month}_{args.year}.csv", index=False
+    )
 
     finish = time.perf_counter()
     print(f"Updated user ratings in {finish - start} seconds")
