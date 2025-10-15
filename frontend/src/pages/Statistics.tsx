7ee97b20a53160202ff792042d3e053513e5c2bb
Victor Verma <victor.verma@hotmail.com>
2025-10-14 23:18:21 -0400

    put ads

--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -14,6 +14,9 @@ import LinearIndeterminate from "../components/LinearIndeterminate";
 import PageTitle from "../components/Layout/PageTitle";
 import PercentilesDisplay from "../components/Displays/PercentilesDisplay";
 import StatsTable from "../components/Tables/StatsTable";
+import SquareAd from "../components/Ads/SquareAd";
+
+import useIsScreenLg from "../hooks/useIsScreenLg";
 
 import {
     StatisticsFormValues,
@@ -63,6 +66,7 @@ const additionalStatsDefinitions = [
 ];
 
 const Statistics = () => {
+    const isScreenLg = useIsScreenLg();
     const { enqueueSnackbar } = useSnackbar();
     const [currentUser, setCurrentUser] = useState("");
     const [statistics, setStatistics] = useState<null | StatisticsResponse>(
@@ -127,135 +131,151 @@ const Statistics = () => {
     };
 
     return (
-        <div>
-            <Helmet>
-                <title>Letterboxd Profile Statistics</title>
-                <link
-                    rel="canonical"
-                    href="https://recommendations.victorverma.com/statistics"
-                />
-            </Helmet>
-
-            <PageTitle title="Letterboxd User Statistics" />
-
-            <div className="hidden md:block my-16">
-                <CycleText
-                    texts={[
-                        "How does your profile compare to other Letterboxd users?",
-                        "What are your highest and lowest rated genres?",
-                        "What is the distribution of your Letterboxd ratings?",
-                    ]}
-                    cycleTime={4000}
-                />
-            </div>
-
-            {!gettingStatistics && (
-                <form
-                    className="w-fit mx-auto mt-16 flex flex-col space-y-4"
-                    onSubmit={handleSubmit(onSubmit, onError)}
-                    noValidate
-                >
-                    <label
-                        className="text-center text-xl text-palette-darkbrown"
-                        htmlFor="username"
-                    >
-                        Enter Letterboxd Username
-                    </label>
-                    <div className="form-control flex flex-col">
-                        <input
-                            className="w-64 sm:w-96 mx-auto p-1 text-center rounded-md bg-gray-200"
-                            type="text"
-                            id="username"
-                            placeholder="One username only"
-                            {...register("username", {
-                                required: {
-                                    value: true,
-                                    message: "Username is required",
-                                },
-                                validate: {
-                                    notEmpty: (fieldValue) => {
-                                        return (
-                                            fieldValue.trim() !== "" ||
-                                            "username cannot be empty"
-                                        );
-                                    },
-                                    notDefault: (fieldValue) => {
-                                        return (
-                                            fieldValue !==
-                                                "Enter Letterboxd Username" ||
-                                            "must enter username"
-                                        );
-                                    },
-                                },
-                            })}
-                        />
-                        <p className="mx-auto mt-2 text-red-500">
-                            {errors.username?.message}
-                        </p>
-                    </div>
-                    {isDirty && isValid && !gettingStatistics && (
-                        <button className="block mx-auto p-2 rounded-md hover:shadow-md bg-gray-200 hover:bg-palette-lightbrown">
-                            Get Statistics
-                        </button>
-                    )}
-                </form>
-            )}
-
-            {gettingStatistics && (
-                <div className="w-fit mx-auto">
-                    <p className="w-fit mx-auto mt-16 mb-4 md:my-4 sm:text-xl text-palette-darkbrown">
-                        Calculating statistics...
-                    </p>
-                    <LinearIndeterminate />
+        <div className="flex gap-4">
+            {isScreenLg && (
+                <div className="w-full">
+                    <SquareAd />
                 </div>
             )}
-
-            {!gettingStatistics && statistics && (
-                <>
-                    <ExportStatistics
-                        simpleStats={statistics.simple_stats}
-                        percentiles={statistics.percentiles}
-                        currentUser={currentUser}
-                        generatedDatetime={generatedDatetime}
+            <div className="lg:w-[700px] mx-auto bg-red-200">
+                <Helmet>
+                    <title>Letterboxd Profile Statistics</title>
+                    <link
+                        rel="canonical"
+                        href="https://recommendations.victorverma.com/statistics"
                     />
+                </Helmet>
 
-                    <div className="w-9/10 md:w-[640px] mx-auto mt-8">
-                        <StatsTable statistics={statistics.simple_stats} />
-                        <DefinitionsModal
-                            title={"Category Definitions"}
-                            definitions={categoryDefinitions}
-                        />
-                    </div>
+                <PageTitle title="Letterboxd User Statistics" />
 
-                    <PercentilesDisplay percentiles={statistics.percentiles} />
+                <div className="hidden md:block my-16">
+                    <CycleText
+                        texts={[
+                            "How does your profile compare to other Letterboxd users?",
+                            "What are your highest and lowest rated genres?",
+                            "What is the distribution of your Letterboxd ratings?",
+                        ]}
+                        cycleTime={4000}
+                    />
+                </div>
 
-                    <div className="w-9/10 md:w-[640px] mx-auto mt-12">
-                        <GenreStatsTable
-                            statistics={
-                                statistics.simple_stats["genre_averages"]
-                            }
-                        />
-                        <DefinitionsModal
-                            title={"Genre Stats Definitions"}
-                            definitions={additionalStatsDefinitions}
-                        />
-                    </div>
-
-                    <div className="w-9/10 md:w-[640px] mx-auto mt-12">
-                        <div className="mx-auto" id="distribution-chart">
-                            <h3 className="w-fit mx-auto text-md md:text-lg">
-                                {`${currentUser}'s Rating Distribution`}
-                            </h3>
-                            <DistributionChart data={statistics.distribution} />
+                {!gettingStatistics && (
+                    <form
+                        className="w-fit mx-auto mt-16 flex flex-col space-y-4"
+                        onSubmit={handleSubmit(onSubmit, onError)}
+                        noValidate
+                    >
+                        <label
+                            className="text-center text-xl text-palette-darkbrown"
+                            htmlFor="username"
+                        >
+                            Enter Letterboxd Username
+                        </label>
+                        <div className="form-control flex flex-col">
+                            <input
+                                className="w-64 sm:w-96 mx-auto p-1 text-center rounded-md bg-gray-200"
+                                type="text"
+                                id="username"
+                                placeholder="One username only"
+                                {...register("username", {
+                                    required: {
+                                        value: true,
+                                        message: "Username is required",
+                                    },
+                                    validate: {
+                                        notEmpty: (fieldValue) => {
+                                            return (
+                                                fieldValue.trim() !== "" ||
+                                                "username cannot be empty"
+                                            );
+                                        },
+                                        notDefault: (fieldValue) => {
+                                            return (
+                                                fieldValue !==
+                                                    "Enter Letterboxd Username" ||
+                                                "must enter username"
+                                            );
+                                        },
+                                    },
+                                })}
+                            />
+                            <p className="mx-auto mt-2 text-red-500">
+                                {errors.username?.message}
+                            </p>
                         </div>
-                    </div>
+                        {isDirty && isValid && !gettingStatistics && (
+                            <button className="block mx-auto p-2 rounded-md hover:shadow-md bg-gray-200 hover:bg-palette-lightbrown">
+                                Get Statistics
+                            </button>
+                        )}
+                    </form>
+                )}
 
-                    <ExportDistribution
-                        distribution={statistics.distribution}
-                        currentUser={currentUser}
-                        generatedDatetime={generatedDatetime}
-                    />
-                </>
+                {gettingStatistics && (
+                    <div className="w-fit mx-auto">
+                        <p className="w-fit mx-auto mt-16 mb-4 md:my-4 sm:text-xl text-palette-darkbrown">
+                            Calculating statistics...
+                        </p>
+                        <LinearIndeterminate />
+                    </div>
+                )}
+
+                {!gettingStatistics && statistics && (
+                    <>
+                        <ExportStatistics
+                            simpleStats={statistics.simple_stats}
+                            percentiles={statistics.percentiles}
+                            currentUser={currentUser}
+                            generatedDatetime={generatedDatetime}
+                        />
+
+                        <div className="w-9/10 md:w-[640px] mx-auto mt-8">
+                            <StatsTable statistics={statistics.simple_stats} />
+                            <DefinitionsModal
+                                title={"Category Definitions"}
+                                definitions={categoryDefinitions}
+                            />
+                        </div>
+
+                        <PercentilesDisplay
+                            percentiles={statistics.percentiles}
+                        />
+
+                        <div className="w-9/10 md:w-[640px] mx-auto mt-12">
+                            <GenreStatsTable
+                                statistics={
+                                    statistics.simple_stats["genre_averages"]
+                                }
+                            />
+                            <DefinitionsModal
+                                title={"Genre Stats Definitions"}
+                                definitions={additionalStatsDefinitions}
+                            />
+                        </div>
+
+                        <div className="w-9/10 md:w-[640px] mx-auto mt-12">
+                            <div className="mx-auto" id="distribution-chart">
+                                <h3 className="w-fit mx-auto text-md md:text-lg">
+                                    {`${currentUser}'s Rating Distribution`}
+                                </h3>
+                                <DistributionChart
+                                    data={statistics.distribution}
+                                />
+                            </div>
+                        </div>
+
+                        <ExportDistribution
+                            distribution={statistics.distribution}
+                            currentUser={currentUser}
+                            generatedDatetime={generatedDatetime}
+                        />
+                    </>
+                )}
+            </div>
+            {isScreenLg && (
+                <div className="w-full">
+                    <SquareAd />
+                </div>
             )}
         </div>
     );
