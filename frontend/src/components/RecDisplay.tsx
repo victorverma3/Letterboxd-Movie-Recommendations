04e0afa0013e8f3d51ee55e5329450813cf9e077
Victor Verma <victor.verma@hotmail.com>
2025-10-14 21:43:39 -0400

    improved sizing

--- frontend/src/components/RecDisplay.tsx
+++ frontend/src/components/RecDisplay.tsx
@@ -8,7 +8,7 @@ interface RecDisplayProps {
 
 const RecDisplay = ({ recommendations }: RecDisplayProps) => {
     return (
-        <div className="max-h-128 overflow-y-scroll overflow-x-hidden w-fit max-w-5xl mt-4 mx-auto py-4 px-2 flex flex-wrap gap-4 justify-evenly border-2 border-gray-200 bg-palette-lightbrown">
+        <div className="max-h-128 overflow-y-scroll overflow-x-hidden w-fit max-w-4/5 md:max-w-[700px] sm:w-full mt-4 mx-auto py-4 px-2 flex flex-row flex-wrap gap-2 justify-evenly border-2 rounded-lg border-gray-200 bg-palette-lightbrown">
             {recommendations.map((rec) => (
                 <MovieCard key={rec.url} recommendation={rec} />
             ))}
