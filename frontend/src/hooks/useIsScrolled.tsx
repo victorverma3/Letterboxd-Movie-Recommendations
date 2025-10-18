26bacfb657b95e5e327a2e6616ab36b22bed1541
Victor Verma <victor.verma@hotmail.com>
2025-10-17 21:59:44 -0400

    added scroll hook

---
+++ frontend/src/hooks/useIsScrolled.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+const useIsScrolled = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        handleScroll();
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return isScrolled;
+};
+
+export default useIsScrolled;
